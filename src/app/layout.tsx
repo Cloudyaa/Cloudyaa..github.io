8cc58c4adb1915600be6304e6c52778e40e11773
Cloudyaa <klaudiaaolczyk@gmail.com>
2023-05-12 23:50:37 +0100

    Refactor code with prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,25 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import React from "react";
-import {Layout} from "@/types";
-import {Navigation} from "@/components";
+import './globals.css';
+import { Inter } from 'next/font/google';
+import React from 'react';
+import { Layout } from '@/types';
+import { Navigation } from '@/components';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Klaudia Olczyk personal portfolio',
   description: 'Personal portfolio page',
-}
+};
 
-const RootLayout = ({children}: Layout) => {
+const RootLayout = ({ children }: Layout) => {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <Navigation/>
-      {children}
+        <Navigation />
+        {children}
       </body>
     </html>
-  )
-}
+  );
+};
 
 export default RootLayout;
