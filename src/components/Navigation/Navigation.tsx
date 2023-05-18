a8c3912866e64c9a03d15a3dc1461fb258d7e600
Cloudyaa <klaudiaaolczyk@gmail.com>
2023-05-18 19:56:52 +0100

    Refactor code with prettier

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,29 +1,24 @@
 'use client';
 
 import React from 'react';
-import {usePathname} from "next/navigation";
-import Link from "next/link";
-import {navLinks} from "./navLinks";
-
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import { navLinks } from './navLinks';
 
 export const Navigation = () => {
   const pathname = usePathname();
 
   return (
-      <>
-        {navLinks.map(link => {
-          const isActive = pathname?.startsWith(link.href);
+    <>
+      {navLinks.map((link) => {
+        const isActive = pathname?.startsWith(link.href);
 
-          return (
-              <Link
-                  className={isActive ? 'active' : ''}
-                  href={link.href}
-                  key={link.name}
-              >
-                {link.name}
-              </Link>
-          )
-        })}
-      </>
+        return (
+          <Link className={isActive ? 'active' : ''} href={link.href} key={link.name}>
+            {link.name}
+          </Link>
+        );
+      })}
+    </>
   );
 };
